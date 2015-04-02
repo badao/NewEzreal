92cb92f9d700ad4ecac8caf4f90562ce9cbb0f92
badao <huy_vu626@yahoo.com>
2015-04-02 22:29:15 +0700

    Ezreal
    Ezreal

---
+++ NewEzreal/Combo.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Color = System.Drawing.Color;
+using SharpDX;
+using LeagueSharp;
+using LeagueSharp.Common;
+using LeagueSharp.Common.Data;
+
+namespace NewEzreal
+{
+    class Combo
+    {
+        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+        public static void combo()
+        {
+            if(Program.Q.IsReady() && !Player.IsWindingUp)
+            {
+                var target = TargetSelector.GetTarget(Program.Q.Range, TargetSelector.DamageType.Physical);
+                if (target != null && target.IsValidTarget() && !target.IsZombie && Program.Menu.Item("Use Q Combo").GetValue<bool>())
+                {
+                    var pred = Program.Q.GetPrediction(target); if (pred.Hitchance >= HitChance.Medium) { Program.Q.Cast(target); }
+                    else { foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy)) { if (hero != null && hero.IsValidTarget() && !hero.IsZombie) Program.Q.Cast(hero); } }
+                }
+                
+            }
+            if (Program.W.IsReady() && !Player.IsWindingUp)
+            {
+                var target = TargetSelector.GetTarget(Program.W.Range, TargetSelector.DamageType.Magical);
+                if (target != null && target.IsValidTarget() && !target.IsZombie && Program.Menu.Item("Use W Combo").GetValue<bool>())
+                {
+                    var pred = Program.W.GetPrediction(target); if (pred.Hitchance >= HitChance.Low) { Program.W.Cast(target); }
+                    else { foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy)) { if (hero != null && hero.IsValidTarget() && !hero.IsZombie)Program.W.Cast(hero);  } }
+                }
+            }
+            if (Program.R.IsReady() && !Player.IsWindingUp)
+            {
+                var target = TargetSelector.GetTarget(1500, TargetSelector.DamageType.Magical);
+                if(target != null && target.IsValidTarget() && !target.IsZombie && Program.Menu.Item("Use R Combo").GetValue<bool>())
+                { var x = Program.Menu.Item("Use R If Hit X Enemy").GetValue<Slider>().Value; Program.R.CastIfWillHit(target,x);}
+            }
+            if (ItemData.Bilgewater_Cutlass.GetItem().IsReady())
+            {
+                var target = TargetSelector.GetTarget(ItemData.Bilgewater_Cutlass.GetItem().Range, TargetSelector.DamageType.Physical);
+                if (target != null && target.IsValidTarget() &&!target.IsZombie)
+                    ItemData.Bilgewater_Cutlass.GetItem().Cast(target);
+            }
+            if (ItemData.Blade_of_the_Ruined_King.GetItem().IsReady())
+            {
+                var target = TargetSelector.GetTarget(ItemData.Blade_of_the_Ruined_King.GetItem().Range, TargetSelector.DamageType.Physical);
+                if (target != null && target.IsValidTarget() && !target.IsZombie)
+                    ItemData.Blade_of_the_Ruined_King.GetItem().Cast(target);
+            }
+        }
+        public static void harass()
+        {
+            if (Program.Q.IsReady()&& !Player.IsWindingUp)
+            {
+                var target = TargetSelector.GetTarget(Program.Q.Range, TargetSelector.DamageType.Physical);
+                if (target != null && target.IsValidTarget() && !target.IsZombie && Program.Menu.Item("Use Q Harass").GetValue<bool>() && Player.Mana / Player.MaxMana * 100 >= Program.Menu.Item("minimum Mana HR").GetValue<Slider>().Value)
+                {
+                    var pred = Program.Q.GetPrediction(target); if (pred.Hitchance >= HitChance.Medium) { Program.Q.Cast(target); }
+                    else { foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy)) { if (hero != null && hero.IsValidTarget() && !hero.IsZombie)Program.Q.Cast(hero);  } }
+                }
+            }
+            if (Program.W.IsReady() && !Player.IsWindingUp)
+            {
+                var target = TargetSelector.GetTarget(Program.W.Range, TargetSelector.DamageType.Physical);
+                if (target != null && target.IsValidTarget() && !target.IsZombie && Program.Menu.Item("Use W Harass").GetValue<bool>() && Player.Mana / Player.MaxMana * 100 >= Program.Menu.Item("minimum Mana HR").GetValue<Slider>().Value)
+                {
+                    var pred = Program.W.GetPrediction(target); if (pred.Hitchance >= HitChance.Medium) { Program.W.Cast(target); }
+                    else { foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy)) { if (hero != null && hero.IsValidTarget() && !hero.IsZombie)Program.W.Cast(hero); } }
+                }
+            }
+        }
+        public static void laneclear()
+        {
+            if (Program.Q.IsReady() && !Player.IsWindingUp)
+            {
+                var target = MinionManager.GetMinions(Player.Position, Program.Q.Range, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.Health).FirstOrDefault();
+                if (Program.Menu.Item("Use Q LaneClear").GetValue<bool>() && Player.Mana / Player.MaxMana * 100 > Program.Menu.Item("minimum Mana LC").GetValue<Slider>().Value && target != null && target.IsValidTarget() && !target.IsZombie && !Player.IsWindingUp)
+                { Program.Q.Cast(target); }
+            }
+        }
+        public static void jungclear()
+        {
+            if (Program.Q.IsReady() && !Player.IsWindingUp)
+            {
+                var target = MinionManager.GetMinions(Player.Position, 600, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth).FirstOrDefault();
+                if (Program.Menu.Item("Use Q JungClear").GetValue<bool>() && Player.Mana / Player.MaxMana * 100 > Program.Menu.Item("minimum Mana JC").GetValue<Slider>().Value && target != null && target.IsValidTarget() && !target.IsZombie && !Player.IsWindingUp)
+                { Program.Q.Cast(target); }
+            }
+        }
+        public static void killsteal()
+        {
+            if (Program.Q.IsReady() && Program.Menu.Item("Use Q KillSteal").GetValue<bool>() && !Player.IsWindingUp)
+            {
+               foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy && hero.IsValidTarget(Program.Q.Range)))
+               {
+                   var dmg = Program.Dame(hero, SpellSlot.Q);
+                   if (hero != null && hero.IsValidTarget() && !hero.IsZombie && dmg > hero.Health) { Program.Q.Cast(hero); }
+               }
+            }
+            if (Program.W.IsReady() && Program.Menu.Item("Use W KillSteal").GetValue<bool>() && !Player.IsWindingUp)
+            {
+                foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy && hero.IsValidTarget(Program.W.Range)))
+                {
+                    var dmg = Program.Dame(hero, SpellSlot.W);
+                    if (hero != null && hero.IsValidTarget() && !hero.IsZombie && dmg > hero.Health) { Program.W.Cast(hero); }
+                }
+            }
+            if (Program.R.IsReady() && Program.Menu.Item("Use R KillSteal").GetValue<bool>() && !Player.IsWindingUp)
+            {
+                var x = Program.Menu.Item("R Range KillSteal").GetValue<Slider>().Value;
+                foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy && hero.IsValidTarget(x)))
+                {
+                    var dmg = Program.Dame(hero, SpellSlot.R)*70/100;
+                    if (hero != null && hero.IsValidTarget() && !hero.IsZombie && dmg > hero.Health) { Program.R.Cast(hero); }
+                }
+            }
+        }
+        public static void auto()
+        {
+            if (Program.Q.IsReady() && Program.Menu.Item("AutoQ").GetValue<KeyBind>().Active && !Player.IsWindingUp)
+            {
+                bool x = Program.Menu.Item("Keep Mana for E").GetValue<bool>();
+                if (x == true && Player.Mana < Program.Q.Instance.ManaCost + Program.E.Instance.ManaCost)
+                    return;
+                else
+                {
+                    var target = TargetSelector.GetTarget(Program.Q.Range, TargetSelector.DamageType.Physical);
+                    if (target != null && target.IsValidTarget() && !target.IsZombie)
+                    {
+                        var pred = Program.Q.GetPrediction(target); if (pred.Hitchance >= HitChance.Medium) { Program.Q.Cast(target); }
+                        else { foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsEnemy)) { if (hero != null && hero.IsValidTarget() && !hero.IsZombie)Program.Q.Cast(hero); } }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NewEzreal/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Color = System.Drawing.Color;
+using SharpDX;
+using LeagueSharp;
+using LeagueSharp.Common;
+using LeagueSharp.Common.Data;
+
+namespace NewEzreal
+{
+    class Program
+    {
+        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        public static Spell Q, W, E, R;
+
+        public static Menu Menu;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            if (Player.ChampionName != "Ezreal")
+                return;
+
+            Q = new Spell(SpellSlot.Q,1150);
+            W = new Spell(SpellSlot.W,1000);
+            E = new Spell(SpellSlot.E,475);
+            R = new Spell(SpellSlot.R);
+            Q.SetSkillshot(250,60,2000,true,SkillshotType.SkillshotLine);
+            W.SetSkillshot(250, 80, 1600, false, SkillshotType.SkillshotLine);
+            R.SetSkillshot(1000,160,2000,false,SkillshotType.SkillshotLine);
+
+            Menu = new Menu(Player.ChampionName, Player.ChampionName, true);
+            Menu orbwalkerMenu = new Menu("Orbwalker", "Orbwalker");
+            Orbwalker = new Orbwalking.Orbwalker(orbwalkerMenu);
+            Menu.AddSubMenu(orbwalkerMenu);
+            Menu ts = Menu.AddSubMenu(new Menu("Target Selector", "Target Selector")); ;
+            TargetSelector.AddToMenu(ts);
+            Menu spellMenu = Menu.AddSubMenu(new Menu("Spells", "Spells"));
+
+            Menu Harass = spellMenu.AddSubMenu(new Menu("Harass", "Harass"));
+
+            Menu Combo = spellMenu.AddSubMenu(new Menu("Combo", "Combo"));
+
+            Menu LaneClear = spellMenu.AddSubMenu(new Menu("LaneClear", "LaneClear"));
+
+            Menu JungClear = spellMenu.AddSubMenu(new Menu("JungClear", "JungClear"));
+
+            Menu Auto = spellMenu.AddSubMenu(new Menu("Auto","Auto"));
+
+            Menu KillSteal = spellMenu.AddSubMenu(new Menu("KillSteal", "KillSteal"));
+
+            Menu Draw = spellMenu.AddSubMenu(new Menu("Draw", "Draw"));
+
+            Combo.AddItem(new MenuItem("Use Q Combo", "Use Q Combo").SetValue(true));
+            Combo.AddItem(new MenuItem("Use W Combo", "Use W Combo").SetValue(true));
+            Combo.AddItem(new MenuItem("Use R Combo", "Use R Combo").SetValue(true));
+            Combo.AddItem(new MenuItem("Use R If Hit X Enemy", "Use R If Hit X Enemy").SetValue(new Slider(1, 1, 3)));
+            Combo.AddItem(new MenuItem("Use Muramana", "Use Muramana").SetValue(true));
+            Combo.AddItem(new MenuItem("Use Muramana if mana >", "Use Muramana if mana >").SetValue(new Slider(40, 0, 100)));
+            Harass.AddItem(new MenuItem("Use Q Harass", "Use Q Harass").SetValue(true));
+            Harass.AddItem(new MenuItem("Use W Harass", "Use W Harass").SetValue(true));
+            Harass.AddItem(new MenuItem("minimum Mana HR", "minimum Mana HR").SetValue(new Slider(40, 0, 100)));
+            LaneClear.AddItem(new MenuItem("Use Q LaneClear", "Use Q LaneClear").SetValue(true));
+            LaneClear.AddItem(new MenuItem("minimum Mana LC", "minimum Mana LC").SetValue(new Slider(40, 0, 100)));
+            JungClear.AddItem(new MenuItem("Use Q JungClear", "Use Q LaneClear").SetValue(true));
+            JungClear.AddItem(new MenuItem("minimum Mana JC", "minimum Mana JC").SetValue(new Slider(40, 0, 100)));
+            Auto.AddItem(new MenuItem("AutoQ", "AutoQ").SetValue(new KeyBind("H".ToCharArray()[0],KeyBindType.Toggle,false)));
+            Auto.AddItem(new MenuItem("Keep Mana for E", "Keep Mana for E").SetValue(true));
+            KillSteal.AddItem(new MenuItem("Use Q KillSteal", "Use Q KillSteal").SetValue(true));
+            KillSteal.AddItem(new MenuItem("Use W KillSteal", "Use W KillSteal").SetValue(true));
+            KillSteal.AddItem(new MenuItem("Use R KillSteal", "Use R KillSteal").SetValue(true));
+            KillSteal.AddItem(new MenuItem("R Range KillSteal", "R Range KillSteal").SetValue(new Slider(1500, 800, 2500)));
+            Draw.AddItem(new MenuItem("Draw Q", "Draw Q").SetValue(true));
+            Draw.AddItem(new MenuItem("Draw W", "Draw W").SetValue(true));
+            Draw.AddItem(new MenuItem("Draw E", "Draw E").SetValue(true));
+
+            Menu.AddToMainMenu();
+            Drawing.OnDraw += OnDraw;
+            Orbwalking.OnAttack += OnAttack;
+            Game.OnUpdate += Game_OnGameUpdate;
+            Game.PrintChat("This is HuynMi's Ezreal!");
+        }
+        public static void OnAttack(AttackableUnit unit, AttackableUnit target)
+        {
+
+            if (unit.IsMe && Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
+            {
+                if (ItemData.Youmuus_Ghostblade.GetItem().IsReady())
+                    ItemData.Youmuus_Ghostblade.GetItem().Cast();
+                if (ItemData.Muramana.GetItem().IsReady() && Menu.Item("Use Muramana").GetValue<bool>() && !Player.HasBuff("Muramana") && Player.Mana / Player.MaxMana * 100 >= Menu.Item("Use Muramana if mana >").GetValue<Slider>().Value)
+                    ItemData.Muramana.GetItem().Cast();
+                if (ItemData.Muramana.GetItem().IsReady() && Menu.Item("Use Muramana").GetValue<bool>() && Player.HasBuff("Muramana") && Player.Mana / Player.MaxMana * 100 < Menu.Item("Use Muramana if mana >").GetValue<Slider>().Value)
+                    ItemData.Muramana.GetItem().Cast();
+            }
+        }
+        private static void OnDraw(EventArgs args)
+        {
+            if (Player.IsDead)
+                return;
+            if (Menu.Item("Draw Q").GetValue<bool>())
+            {
+                if (Q.IsReady())
+                {
+                    Render.Circle.DrawCircle(Player.Position, Q.Range, Color.Green);
+                }
+                else
+                {
+                    Render.Circle.DrawCircle(Player.Position, Q.Range, Color.Black);
+                }
+            }
+            if (Menu.Item("Draw W").GetValue<bool>())
+            {
+                if (W.IsReady())
+                {
+                    Render.Circle.DrawCircle(Player.Position, W.Range, Color.Yellow);
+                }
+                else
+                {
+                    Render.Circle.DrawCircle(Player.Position, W.Range, Color.Black);
+                }
+            }
+            if (Menu.Item("Draw E").GetValue<bool>())
+            {
+                if (E.IsReady())
+                {
+                    Render.Circle.DrawCircle(Player.Position, E.Range, Color.BlueViolet);
+                }
+                else
+                {
+                    Render.Circle.DrawCircle(Player.Position, E.Range, Color.Black);
+                }
+            }
+        }
+        public static void Game_OnGameUpdate(EventArgs args)
+        {
+            Combo.killsteal();
+            Combo.auto();
+            if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
+            {
+                Combo.combo();
+            }
+            if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed)
+            {
+                Combo.harass();
+
+            }
+            if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear)
+            {
+                Combo.laneclear();
+                Combo.jungclear();
+            }
+            //if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.None || Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LastHit)
+            //{
+            //    Combo.killsteal();
+            //    Combo.auto();
+            //}
+        }
+        public static double Dame(Obj_AI_Base target,SpellSlot x)
+        {
+            if (target != null) { return Player.GetSpellDamage(target, x); } else return 0;
+        }
+
+        public static void checkbuff()
+        {
+            String temp = "";
+            foreach (var buff in Player.Buffs)
+            {
+                temp += (buff.Name + "(" + buff.Count + ")" + ", ");
+            }
+            Game.PrintChat(temp);
+        }
+    }
+}
